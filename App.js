d71acfeb7cd720ea3b697894951e6afba7d684fe
saurabhoatstech <saurabhnegi0007@gmail.com>
2023-10-11 16:23:34 +0530

    added routing

--- App.js
+++ App.js
@@ -2,16 +2,40 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./src/components/Header";
 import Body from "./src/components/Body";
+import About from "./src/components/About";
+import Contact from "./src/components/Contact";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Body />
+      <Outlet />
     </div>
   );
 }
 
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Body />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<App />);
+root.render(<RouterProvider router={appRouter} />);
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import "../../index.css";
 import { LOGO_URL } from "../utils/constants";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
@@ -8,11 +9,16 @@ const Header = () => {
         <img className="logo-img" src={LOGO_URL} alt="logo" />
       </div>
       <div className="menu-items">
-        <li>Home</li>
-        <li>About Us</li>
-        <li>Contact Us</li>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="about">About Us</Link>
+        </li>
+        <li>
+          <Link to="contact">Contact Us</Link>
+        </li>
         <li>Cart</li>
-        <li>test2</li>
       </div>
     </div>
   );
